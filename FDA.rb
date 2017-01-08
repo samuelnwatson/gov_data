a6ccf94e3575254a2d445a005bfc3a132e2bbb81
sam <1002064@gmx.com>
2017-01-08 19:10:03 +0000

    Initial Commit

---
+++ FDA.rb
@@ -0,0 +1,39 @@
+# - Filename: FDA.rb
+# - Author: Sam Watson
+# - Description: Small program accessing the U.S. government FDA API.
+# -   Fetches n-instances of brands resulting in hospitalisation and remaining symptoms.
+# - Last Edit: 8 Jan, 2017
+
+require 'rubygems'
+require 'open-uri'
+require 'json'
+
+def fda_query(n=50)
+  url = "https://api.fda.gov/food/event.json?"
+  query = "search=products.name_brand+reactions=%22hospitalisation%22&limit=#{n}"
+  request = open(url + query)
+end
+
+def parse_json
+  json_data = fda_query.read
+  parsed_data = JSON.parse(json_data, symbolize_names: true)
+  results = parsed_data.delete(:results)
+end
+
+def organise_data
+  data = parse_json
+  organised_results = {}
+  data.each_with_index do |obj, i| 
+    organised_results[data[i][:products][0][:name_brand]] = data[i][:reactions]
+  end
+  organised_results
+end
+
+def display_results
+	organise_data.each do |brand, symptoms|
+		puts "\nBrandname: #{brand}"
+		puts "Symptoms: #{symptoms}"
+	end
+end
+
+display_results
